c99e5bc90c208639862dc34605f9370b5dec8ee2
tbreimer <breimert@union.edu>
2025-02-13 15:06:30 -0500

    cla completed for all modules

--- cmaes_integration/plot_fitness_over_gens.py
+++ cmaes_integration/plot_fitness_over_gens.py
@@ -1,21 +1,38 @@
 """
 Plot fitness over generations given output.csv.
+Takes command line arg of csv filename.
+
+Ex: `python3 plot_fitness_over_gens.py --filename output.csv`
 
 Author: Thomas Breimer
 February 10th, 2025
 """
 
+import argparse
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-FILENAME = "output.csv"
+parser = argparse.ArgumentParser(description='RL')
+
+parser.add_argument(
+    '--filename',  #headless, screen, video, both h, s, v, b
+    help='name of csv file',
+    default="output.csv")
+
+args = parser.parse_args()
+
+filename = args.filename
 
 df = pd.read_csv("output.csv")
 
 plt.figure(figsize=(8, 5))
-plt.plot(np.array(df["generation"]), np.array(df["best_fitness"]),
-         marker='o', linestyle='-', color='b', label="Best Fitness")
+plt.plot(np.array(df["generation"]),
+         np.array(df["best_fitness"]),
+         marker='o',
+         linestyle='-',
+         color='b',
+         label="Best Fitness")
 plt.xlabel("Generation")
 plt.ylabel("Best Fitness")
 plt.title("Fitness Evolution Over Generations")
