44dac373f1fb5f59a2a5de2227d775046ff23aa6
kashyaa2 <kashyaa2@union.edu>
2025-02-12 19:48:23 -0500

    updated unpacking script to reflect actual shape of cmaes output and the correct distribution into snn w&b; updated sprint goals

--- snn/cmaes_out2snn_wb.py
+++ snn/cmaes_out2snn_wb.py
@@ -1,15 +1,34 @@
 import numpy as np
 
+"""
+Some functions are assumed to be imported from whichever file it is in - will be modified later once rest of the project is caught up
+List of such functions:
+- pipeline.get_cmaes_out()
+- pipeline.get_num_actuators()
+
+Also replace hidden layer size and output size to actual numbers instead of `X` and `Y`
+"""
+
 
 # CONSTANT
 
 # Placeholder numbers - for now used numbers from the example SNN described while talking to John
-NUM_SNNS = 8                   # Number of spiking neural networks (actuators)
-NUM_WEIGHTS_PER_SNN = 6        # Number of weight parameters per SNN
-NUM_BIASES_PER_SNN = 2         # Number of bias parameters per SNN
+NUM_SNN = pipeline.get_num_actuators()     # Number of spiking neural networks (actuators)
+INP_SIZE = NUM_SNN - 1                     # Input size - total number of actuators (number of SNNs - 1)
+HIDDEN_SIZE = X
+OUTPUT_SIZE = Y
+
+NUM_WEIGHTS_PER_HIDDEN_NODE = INP_SIZE      # Number of weight parameters per node
+NUM_BIASES_PER_HIDDEN_NODE = 1              # Number of biases per node
+NUM_WEIGHTS_PER_OUTPUT_NODE = HIDDEN_SIZE
+NUM_BIASES_PER_OUTPUT_NODE = 1
 
 # Total number of parameters per SNN is the sum of weights and biases.
-PARAMS_PER_SNN = NUM_WEIGHTS_PER_SNN + NUM_BIASES_PER_SNN
+PARAMS_PER_HIDDEN_LAYER = (NUM_WEIGHTS_PER_HIDDEN_NODE + NUM_BIASES_PER_HIDDEN_NODE) * HIDDEN_SIZE
+PARAMS_PER_OUTPUT_LAYER = (NUM_WEIGHTS_PER_OUTPUT_NODE + NUM_BIASES_PER_OUTPUT_NODE) * OUTPUT_SIZE
+PARAMS_PER_SNN = PARAMS_PER_HIDDEN_LAYER + PARAMS_PER_OUTPUT_LAYER
+
+CMAES_OUT_SIZE = NUM_SNN * PARAMS_PER_SNN
 
 # Mapping voxel (SNN) to SNN parameters in the `snn_param` list
 # VOXEL_SNN_MAPPING = {
@@ -20,14 +39,6 @@ PARAMS_PER_SNN = NUM_WEIGHTS_PER_SNN + NUM_BIASES_PER_SNN
 #     N: "voxel N"
 # }
 
-def get_expected_cmaes_size():
-    """
-    Calculate the expected size of the CMA-ES output vector.
-    
-    Returns:
-        int: The total number of parameters (NUM_SNNS * PARAMS_PER_SNN).
-    """
-    return NUM_SNNS * PARAMS_PER_SNN
 
 def unpack_cmaes_output():
     """
@@ -45,23 +56,31 @@ def unpack_cmaes_output():
     # Assume get_cmaes_out() is provided by another module.
     # For example, one might import it like:
     # from external_module import get_cmaes_out
-    flat_vector = np.array(get_cmaes_out())  # Expected to return a flat array-like object. Function can be added or imported from evogym codebase once the team finishes building it.
     
-    expected_size = get_expected_cmaes_size()
-    if flat_vector.size != expected_size:
-        raise ValueError(f"Expected CMA-ES output vector of size {expected_size}, got {flat_vector.size}.")
+    # Expected to return a flat array-like object. 
+    # Function can be added or imported from evogym codebase once the team finishes building it.
+    flat_vector = np.array(pipeline.get_cmaes_out())
+
+    if flat_vector.size != CMAES_OUT_SIZE:
+        raise ValueError(f"Expected CMA-ES output vector of size {CMAES_OUT_SIZE}, got {flat_vector.size}.")
 
     # Reshape the flat vector to a 2D array: each row corresponds to one SNN.
-    reshaped = flat_vector.reshape((NUM_SNNS, PARAMS_PER_SNN))
+    reshaped = flat_vector.reshape((NUM_SNN, PARAMS_PER_SNN))
     
     # For each SNN, split the parameters into weights and biases.
     snn_parameters = []
-    for params in reshaped:
-        weights = params[:NUM_WEIGHTS_PER_SNN]
-        biases = params[NUM_WEIGHTS_PER_SNN:]
+    for snn in reshaped:
+        hidden_params = []
+        offset = 0
+
+        for _ in range(HIDDEN_SIZE):
+            hidden_weights = snn[offset:(offset + NUM_WEIGHTS_PER_HIDDEN_NODE)]
+            hidden_biases = params[NUM_BIASES_PER_HIDDEN_NODE:]
+            hidden_params.append()
+        
         snn_parameters.append({
-            'weights': weights,
-            'biases': biases
+            'weights': hidden_weights,
+            'biases': hidden_biases
         })
     
     return snn_parameters
