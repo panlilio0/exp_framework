770ad6645d14e2dd9ba3d5b6dab8448ca3704ff5
kashyaa2 <kashyaa2@union.edu>
2025-02-16 15:17:13 -0500

    completed cmaes output unpacking and tested setting weights to snn using dummy cmaes output

--- snn/cmaes_out2snn_wb.py
+++ snn/cmaes_out2snn_wb.py
@@ -15,7 +15,7 @@ Also replace hidden layer size and output size to actual numbers instead of `X` 
 # Placeholder numbers - for now used numbers from the example SNN described while talking to John
 NUM_SNN =  4  # pipeline.get_num_actuators()     # Number of spiking neural networks (actuators)
 INP_SIZE = NUM_SNN - 1                     # Input size - total number of actuators (number of SNNs - 1)
-HIDDEN_SIZE = 1  # X
+HIDDEN_SIZE = 3  # X
 OUTPUT_SIZE = 1  # Y
 
 NUM_WEIGHTS_PER_HIDDEN_NODE = INP_SIZE      # Number of weight parameters per node
@@ -26,6 +26,7 @@ PARAMS_PER_HIDDEN_LAYER = (NUM_WEIGHTS_PER_HIDDEN_NODE + 1) * HIDDEN_SIZE  # 1 =
 PARAMS_PER_OUTPUT_LAYER = (NUM_WEIGHTS_PER_OUTPUT_NODE + 1) * OUTPUT_SIZE  # 1 = number of bias in the node
 PARAMS_PER_SNN = PARAMS_PER_HIDDEN_LAYER + PARAMS_PER_OUTPUT_LAYER
 CMAES_OUT_SIZE = NUM_SNN * PARAMS_PER_SNN
+print(f"CMAES_OUT_SIZE: {CMAES_OUT_SIZE}")
 
 # Mapping voxel (SNN) to SNN parameters in the `snn_param` list
 # VOXEL_SNN_MAPPING = {
@@ -62,29 +63,8 @@ def unpack_cmaes_output(cmaes_out):
     # For each SNN, split the parameters into weights and biases.
     snn_parameters = {}
     for snn_idx, params_per_snn in enumerate(reshaped):
-        hidden_params = []
-        current_node = 0
-        for _ in range(HIDDEN_SIZE):
-            offset = current_node + NUM_WEIGHTS_PER_HIDDEN_NODE
-            hidden_weights = params_per_snn[current_node:offset]
-            hidden_biases = params_per_snn[offset]
-            hidden_params.append({
-                'weights': hidden_weights,
-                'bias': hidden_biases
-            })
-            current_node += PARAMS_PER_HIDDEN_LAYER
-        
-        output_params = []
-        for _ in range(OUTPUT_SIZE):
-            offset = current_node + NUM_WEIGHTS_PER_OUTPUT_NODE
-            output_weights = params_per_snn[current_node:offset]
-            output_biases = params_per_snn[offset]
-            output_params.append({
-                'weights': output_weights,
-                'bias': output_biases
-            })
-            current_node += PARAMS_PER_OUTPUT_LAYER
-        
+        hidden_params = params_per_snn[:PARAMS_PER_HIDDEN_LAYER]
+        output_params = params_per_snn[PARAMS_PER_HIDDEN_LAYER:]
         snn_parameters[snn_idx] = {
             'hidden_layer': hidden_params,
             'output_layer': output_params
@@ -94,19 +74,24 @@ def unpack_cmaes_output(cmaes_out):
 
 
 if __name__ == '__main__':
-    try:
-        snn_params = unpack_cmaes_output(np.random.randint(1, 15, CMAES_OUT_SIZE))
-        for snn_id, params in snn_params.items():
-            print(f"SNN {snn_id}:")
-
-            print("    Hidden Layer:")
-            for i, node in enumerate(params['hidden_layer']):
-                print(f"        Node {i}: Weights = {node['weights']}; Bias = {node['bias']}")
-            
-            print("    Output Layer:")
-            for i, node in enumerate(params['output_layer']):
-                print(f"        Node {i}: Weights = {node['weights']}; Bias = {node['bias']}")
-            
-            print('\n')
-    except Exception as error:
-        print(f"Error while unpacking: {error}")
+    
+    def test():
+        from john_code_snn import SpikyNet
+        try:
+            snns = [SpikyNet(input_size=INP_SIZE, hidden_size=HIDDEN_SIZE, output_size=OUTPUT_SIZE) for _ in range(NUM_SNN)]
+            print("creating snn_params")
+            snn_params = unpack_cmaes_output(np.random.randint(1, 75, CMAES_OUT_SIZE))
+            print("created snn_params")
+            print(snn_params)
+            print("Testing unpack+set:", end='\n')
+            for snn_id, params in snn_params.items():
+                print(f"SNN {snn_id} params: {params}")
+                snns[snn_id].hidden_layer.set_weights(params['hidden_layer'])  # np.concatenate((params['hidden_layer'][0]['weights'], [params['hidden_layer'][0]['bias']]))
+                snns[snn_id].output_layer.set_weights(params['output_layer'])  # np.concatenate((params['output_layer'][0]['weights'], [params['output_layer'][0]['bias']]))
+                print(f"SNN {snn_id}: ")
+                print(snns[snn_id].compute(np.random.randint(1, 15, INP_SIZE)))
+                print('\n')
+        except Exception as error:
+            print(f"Error while unpacking: {error}")
+    
+    test()
