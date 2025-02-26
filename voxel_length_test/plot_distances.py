f4731e6b0d90f613d7eee83c2b93c84632f57d07
James Gaskell <jamesgaskell@Jamess-MacBook-Pro.local>
2025-02-26 12:02:45 -0500

    added latest file for default plot

--- voxel_length_test/plot_distances.py
+++ voxel_length_test/plot_distances.py
@@ -4,10 +4,13 @@ plotting csv files for james.
 
 import os
 import argparse
+import glob
 #import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from pathlib import Path
 
+PARENTDIR = Path(__file__).parent.resolve()
 FILEFOLDER = "distance_outputs"
 
 def plot_all(csv_filename):
@@ -15,7 +18,9 @@ def plot_all(csv_filename):
     Given a csv with distances, plots the distances.
     '''
     #read
-    df = pd.read_csv(csv_filename)
+    folder_path = os.path.join(PARENTDIR, FILEFOLDER)
+
+    df = pd.read_csv(folder_path + "/" + csv_filename)
 
     #start plot
     plottitle = csv_filename[:-4] + " actions"
@@ -30,16 +35,29 @@ def plot_all(csv_filename):
         plt.plot(range(len(to_plot)), to_plot, label=col)
 
     plt.legend(loc='upper center')
-    plt.savefig(os.path.join(FILEFOLDER, plottitle + '.png'))
+    plt.savefig(os.path.join(folder_path, plottitle + '.png'))
     plt.close()
 
+def get_latest_file():
+    """
+    Finds the most recently created file in a folder matching a given pattern.
+    
+    Returns:
+        str: The path to the most recently created file, or None if no files match the pattern.
+    """
+    this_dir = Path(__file__).parent.resolve()
+    files = glob.glob(os.path.join(this_dir, FILEFOLDER) + "/*.csv")
+    if not files:
+        return None
+    latest_file = max(files, key=os.path.getctime).split("/")[-1]
+    return latest_file
 
 
 if __name__ == "__main__":
 
     #args
     PARSER = argparse.ArgumentParser(description="Arguments for simple experiment.")
-    PARSER.add_argument("csv_filename", type=str,
+    PARSER.add_argument("--csv_filename", type=str, default=get_latest_file(),
                         help="The csv file of the robot distances you want to plot.")
 
     ARGS = PARSER.parse_args()
