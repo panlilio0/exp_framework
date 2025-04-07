306be812aff02ea20fbd52b61028f749230fbb8e
James Gaskell <gaskellj@union.edu>
2025-04-07 13:50:59 -0400

    added options for graphing spikes and levels after a visualization

--- cmaes_framework/best_individual_latest.py
+++ cmaes_framework/best_individual_latest.py
@@ -9,6 +9,9 @@ import os
 from pathlib import Path
 import pandas
 import time
+import argparse
+import numpy as np
+from matplotlib import pyplot as plt
 from snn_sim.run_simulation import run
 
 ITERS = 1000
@@ -18,7 +21,7 @@ PARENTDIR = Path(__file__).parent.parent.resolve()
 GENOME_START_INDEX = 3
 FILEFOLDER = "data"
 
-def visualize_best(filename="latest.csv"):
+def visualize_best(graphs, filename="latest.csv"):
     """
     Look at a csv and continuously run best individual.
     
@@ -40,10 +43,52 @@ def visualize_best(filename="latest.csv"):
             row = df.loc[df['best_fitness'] == best_fitness]
             genome = row.values.tolist()[0][GENOME_START_INDEX:]
 
-            run(ITERS, genome, "s")
+            _, spikes, levels = run(ITERS, genome, "s")
+
+            spikes = np.array(spikes)
+            levels = np.array([[x[0] for x in row] for row in levels])
+
+        if graphs == "s":
+
+            fig, ax = plt.subplots(figsize=(12, 5))
+
+            for neuron_idx in range(spikes.shape[1]):
+                spike_times = np.where(spikes[:, neuron_idx])[0]
+                ax.vlines(spike_times, neuron_idx - 0.4, neuron_idx + 0.4)
+
+            ax.set_yticks(np.arange(spikes.shape[1]))
+            ax.set_yticklabels([f'Neuron {i}' for i in range(spikes.shape[1])])
+            ax.set_xlabel("Time Steps")
+            ax.set_ylabel("Neuron Index")
+            ax.set_title("Spike Train of Neurons")
+            plt.tight_layout()
+            plt.xlim(0, 100)
+            plt.show(block=True)
+        
+        elif graphs == "l":
+
+            fig, ax = plt.subplots(figsize=(12, 6))
+
+            for i in range(levels.shape[1]):
+                ax.plot(levels[:, i], label=f'Neuron {i}')
+
+            ax.set_xlabel('Time Steps')
+            ax.set_ylabel('Level')
+            ax.set_title('Neuron Levels Over Time')
+            ax.legend(loc='upper right')
+            plt.tight_layout()
+            plt.show(block=True)
 
 if __name__ == "__main__":
-    visualize_best()
+    parser = argparse.ArgumentParser(description='RL')
+    parser.add_argument(
+        '--graphs',
+        help='graph outputs and levels? s - spike trains, l - levels, y - both',
+        default="l")
+    
+    args = parser.parse_args()
+    
+    visualize_best(args.graphs)
 
         
     
