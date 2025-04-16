aab888c329c19c09024eb10b73be5bd497000a81
James Gaskell <gaskellj@union.edu>
2025-04-16 15:03:00 -0400

    reverted silly input scaling

--- cmaes_framework/snn_sim/run_simulation.py
+++ cmaes_framework/snn_sim/run_simulation.py
@@ -121,16 +121,6 @@ def run(iters, genome, mode, vid_name=None, vid_path=None, snn_logs=False, log_f
     snn_controller = SNNController(2, 2, 1, robot_config=robot_file_path)
     snn_controller.set_snn_weights(genome)
 
-    def scale_inputs(last_distance, cur_distances):
-        epsilon = 1e-6
-        scaled = (last_distance - cur_distances) / (last_distance + epsilon)
-
-        min_val = scaled.min()
-        max_val = scaled.max()
-        normalized = ((scaled - min_val) / (max_val - min_val + epsilon)) # Should we divide by something here?
-    
-        return normalized
-
 
     for i in range(iters):
         # Get point mass locations
@@ -139,19 +129,8 @@ def run(iters, genome, mode, vid_name=None, vid_path=None, snn_logs=False, log_f
         # Get current corner distances
         corner_distances = np.array(morphology.get_corner_distances(raw_pm_pos))
 
-        # First iteration: set up initial distances
-        if i == 0:
-            init_distances = corner_distances.copy()
-            scaled_inputs = scale_inputs(init_distances, corner_distances)
-        else:
-            scaled_inputs = scale_inputs(last_distance, corner_distances)
-
         # Get action from SNN controller
-        lengths = snn_controller.get_lengths(scaled_inputs)
-        action = lengths
-
-        # Update last_distance for next iteration
-        last_distance = corner_distances.copy()
+        action = snn_controller.get_lengths(corner_distances)
   
         # Clip actuator target lengths to be between 0.6 and 1.6 to prevent buggy behavior
         action = np.clip(action, ACTUATOR_MIN_LEN, ACTUATOR_MAX_LEN)
@@ -174,7 +153,6 @@ def run(iters, genome, mode, vid_name=None, vid_path=None, snn_logs=False, log_f
     viewer.close()
 
     # Get robot point mass position position afer sim has run
-
     final_raw_pm_pos = sim.object_pos_at_time(sim.get_time(), "robot")
 
     fitness = np.mean(final_raw_pm_pos[0]) - np.mean(init_raw_pm_pos[0])
@@ -184,13 +162,6 @@ def run(iters, genome, mode, vid_name=None, vid_path=None, snn_logs=False, log_f
 
     if snn_logs:
         snn_controller.generate_output_csv(log_filename)
-
-    # If we can fix falling over exploit we may be able to do 100 iters for CMA-ES then run it for longer
-    # bottom_pos = final_raw_pm_pos[1][-4:]
-    # for val in bottom_pos: # Fix falling over in fitness
-    #    if val > 1.6:
-    #        if not np.mean(final_raw_pm_pos[1]) - np.mean(init_raw_pm_pos[1]) > 0.6: # Checks if robot is airborne so we don't get rid of jumping bots
-    #            fitness = 0
 
     return FITNESS_OFFSET - fitness # Turn into a minimization problem
 
